96480319cf0b546667db02fe3f8b845df196e6f5
ba-ddou <ba-ddou@users.noreply.github.com>
2020-02-01 12:37:24 +0100

    footer basic layout

--- src/app/index.js
+++ src/app/index.js
@@ -2,8 +2,8 @@ import React, { Component } from 'react'
 import { observer, inject, Provider } from 'mobx-react'
 import './styles/index.scss'
 import { BrowserRouter, Route } from 'react-router-dom';
-import Header from './components/Header'
-
+import Header from 'app/components/Header'
+import Footer from 'app/components/Footer'
 export default class App extends Component {
 
 
@@ -13,10 +13,11 @@ export default class App extends Component {
         
         <BrowserRouter>
           <Header />
-          <Route exact path='/' render={(props) =><div style={{height : 'calc(100% - 160px)'}}><h1 className="center-XY">novagency.io | About</h1></div> } />
-          <Route exact path='/projects' render={(props) => <div style={{height : 'calc(100% - 160px)'}}><h1 className="center-XY">novagency.io | Projects</h1></div>} />
-          <Route exact path='/contact' render={(props) => <div style={{height : 'calc(100% - 160px)'}}><h1 className="center-XY">novagency.io | Contact</h1></div>} />
+          <Route exact path='/' render={(props) =><div style={{height : 'calc(100% - 220px)'}}><h1 className="center-XY">novagency.io | About</h1></div> } />
+          <Route exact path='/projects' render={(props) => <div style={{height : 'calc(100% - 220px)'}}><h1 className="center-XY">novagency.io | Projects</h1></div>} />
+          <Route exact path='/contact' render={(props) => <div style={{height : 'calc(100% - 220px)'}}><h1 className="center-XY">novagency.io | Contact</h1></div>} />
         </BrowserRouter>
+        <Footer />
       </Provider>
     )
   }

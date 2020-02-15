db123ac2889c9f1b020edc4135fc4ce0c9f3eff6
ba-ddou <ba-ddou@users.noreply.github.com>
2020-02-15 13:14:45 +0100

    initiated Project page

---
+++ src/app/screens/project/index.js
@@ -0,0 +1,45 @@
+/*
+*
+*
+* Project page
+*
+*/
+
+
+import React from 'react';
+import { useParams } from 'react-router-dom';
+import { inject } from 'mobx-react';
+import {toJS} from 'mobx'
+import ProjectsNavBar from './components/ProjectsNavBar'
+import ProjectControls from './components/ProjectControls'
+
+const Project = inject('modelStore')(
+    (props) => {
+        let { projectName } = useParams();
+        let project = props.modelStore.getProject(projectName);
+        return (
+            <div id="projectPage">
+                <ProjectsNavBar />
+                <div className="projectPage-info">
+                    <div className="projectPage-info-services">
+                        {project[0].services.map(
+                            service=><span className="projectPage-info-service" >{service}</span>
+                        )}
+                    </div>
+                        <div className="projectPage-info-description">{project[0].description}</div>
+                </div>
+                <div className="projectPage-images">
+                    {project[0].images.map(
+                        image => <img className="projectPage-images-image" src={image.src} alt={image.alt} />
+                    )}
+                </div>
+                <ProjectControls />
+            </div>
+
+        );
+    }
+);
+
+export default Project;
+
+
--- src/app/screens/projects/index.js
+++ src/app/screens/projects/index.js
@@ -10,6 +10,7 @@ import React from 'react';
 import './styles.scss'
 import { BrowserRouter, Route, useParams, useRouteMatch } from 'react-router-dom';
 import ProjectsCarousel from './components/ProjectsCarousel'
+import Project from 'app/screens/project'
 
 const Projects = () => {
     // get the current Route's path
@@ -34,7 +35,3 @@ const Projects = () => {
 export default Projects;
 
 
-var Project = (props) => {
-    let { projectName } = useParams();
-    return <div><h3 className="center-XY">novagency.io | {projectName} </h3></div>
-}

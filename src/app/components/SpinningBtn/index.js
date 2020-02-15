38639374a333a35811be16b64302fe41f7a865b3
ba-ddou <ba-ddou@users.noreply.github.com>
2020-02-15 13:39:56 +0100

    spin-fix svgSpinningBtn

--- src/app/components/SpinningBtn/index.js
+++ src/app/components/SpinningBtn/index.js
@@ -6,74 +6,40 @@
 */
 
 import React from 'react';
-import './styles.scss'
+import './styles.sass'
 import { motion, useAnimation } from 'framer-motion'
 
 const SpinWrapper = (props) => {
-    const controls = useAnimation();
-    let start = async () =>{
-        
-        controls.start({
-            rotate: 360,
-            transition: {
+    return (
+        <motion.div
+            className="spinWrapper"
+            animate={{ rotate: 360 }}
+            transition={{
                 duration: 7,
                 ease: 'linear',
                 loop: Infinity
-            }
-        });
-        // console.log('start');
-        
-    }
-
-    let stop = async () => {
-        await controls.stop();
-        // console.log('stop');
-    }
-    start();
-
-
-    return (
-        <div id="spinWrapper">
-            <motion.div
-                className="spin"
-                animate={controls}
-                // onHoverStart={}
-                // onHoverEnd={}
-            >
+            }}
+        >
             {props.children}
-            </motion.div>
-        </div>
+        </motion.div>
     );
 }
 
-export const SvgSpinningBtn = ({src})=>{
+export const SvgSpinningBtn = ({ spin, fix }) => {
     return (
-        <SpinWrapper>
-            <img src={src} />
-        </SpinWrapper>
+        <div className="svgSpinningBtn">
+            <SpinWrapper>
+                <img src={spin} />
+            </SpinWrapper>
+            <img src={fix} />
+        </div>
+
     )
 }
 
 
 
-export const ArrowSpinningBtn = (props) => {
-    return (
-        <SpinWrapper>
-            <span className="arrowSpinningBtn center-XY">
-                <i className={`icon ion-ios-arrow-round-${props.direction}`}></i>
-            </span>
-        </SpinWrapper>
 
-    );
-}
-
-export const SendSpinningBtn = (props) => {
-    return (
-        <div className="spinningBtn">
-            Send
-        </div>
-    );
-}
 
 
 
--- src/app/screens/projects/components/CarouselControls/index.js
+++ src/app/screens/projects/components/CarouselControls/index.js
@@ -11,17 +11,18 @@ import './styles.scss'
 // Import the Arrow Spinning Button from the SpinningBtn package
 import { ArrowSpinningBtn , SvgSpinningBtn } from 'app/components/SpinningBtn';
 import img from 'app/assets/images/xdesign.jpg';
+import apple from 'app/assets/images/apple.svg'
 
 const CarouselControls = ({next,prev}) => {
     return (
         <div id="carouselControls">
             <div id="carouselControls-left" onClick={prev}>
                 {/* <ArrowSpinningBtn direction="back" /> */}
-                <SvgSpinningBtn src={img} />
+                <SvgSpinningBtn spin={img} fix={apple} />
             </div>
             <div id="carouselControls-right" onClick={next}>
                 {/* <ArrowSpinningBtn direction="forward" /> */}
-                <SvgSpinningBtn src={img} />
+                <SvgSpinningBtn spin={img} fix={apple} />
             </div>
 
         </div>

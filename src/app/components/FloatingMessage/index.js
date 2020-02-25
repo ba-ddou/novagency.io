15b28faadc4259f6d12f465dc4544697ad20d7f4
ba-ddou <ba-ddou@users.noreply.github.com>
2020-02-25 20:57:32 +0100

    refined submit error messages, and added a timeout gate

--- src/app/components/FloatingMessage/index.js
+++ src/app/components/FloatingMessage/index.js
@@ -1,25 +1,22 @@
 /*
-*
-*
-* a floating message for form submition feedback
-*
-*
-*/
+ *
+ *
+ * a floating message for form submition feedback
+ *
+ *
+ */
 
+import React from "react";
+import ReactDOM from "react-dom";
+import "./styles.sass";
 
-import React from 'react';
-import ReactDOM from 'react-dom'
-import './styles.sass'
-
-
-const FloatingMessage = ({content}) => {
-    console.log("floatingmessage");
-    return ReactDOM.createPortal((
-        <div className="floatingMessage">
-            <span>{content}</span>
-        </div>
-    )
-    ,document.getElementById("root"));
-}
+const FloatingMessage = ({ content }) => {
+	return ReactDOM.createPortal(
+		<div className="floatingMessage">
+			<span>{content}</span>
+		</div>,
+		document.getElementById("root")
+	);
+};
 
 export default FloatingMessage;
--- src/app/lib/services.js
+++ src/app/lib/services.js
@@ -1,117 +1,124 @@
 /*
-*
-*
-* Services layer
-*
-*
-*/
+ *
+ *
+ * Services layer
+ *
+ *
+ */
 
 // import * as firebase from 'firebase/app'
 // import 'firebase/firestore';
 
+export default new (class services {
+	submitInquiry = async values => {
+		try {
+			let db = firebase.firestore();
+			let { timeout, id, ...error } = await race(
+				db.collection("inquiries").add(values)
+			);
 
-export default new class services {
+			if (id) {
+				console.log(id);
+				return ["inquiry successfully sent", false];
+			} else if (timeout) {
+				console.log("request timeout");
+				return [false, "connection error"];
+			} else {
+				console.error(error);
+				return [false, "unknown error, try agian later"];
+			}
+		} catch (error) {
+			await new Promise((resolve, reject) => setTimeout(resolve, 1000));
+			return ["mock inquiry was succefully sent", false];
+		}
+	};
 
+	getProjects = async () => {
+		await new Promise((resolve, reject) => setTimeout(resolve, 200));
+		return projects;
+	};
+})();
 
-
-    submitInquiry = async (values) => {
-        try {
-            let db = firebase.firestore();
-            let { id, ...error } = await db.collection('inquiries').add(values);
-            if (id) {
-                console.log(id);
-                return [`inquiry saved successfully | With id ${id}`,false]
-            }
-            else {
-                console.log(error);
-                return [false,'there was a problem submiting your inquiry']
-            }
-        } catch (error) {
-            await new Promise((resolve, reject) => setTimeout(resolve, 1000));
-            return ['mock inquiry was succefully saved',false]
-        }
-
-
-    }
-
-    getProjects = async () => {
-        await new Promise((resolve, reject) => setTimeout(resolve, 200));
-        return projects;
-    }
+function race(request) {
+	return Promise.race([
+		request,
+		new Promise((resolve, reject) =>
+			setTimeout(_ => resolve({ timeout: true }), 3000)
+		)
+	]);
 }
 
-
 const projects = [
-    {
-        name: "Branding",
-        tagline: "visual Identity and branding",
-        description: "visual Identity and branding",
-        services: ["visual Identity", "Print", "branding"],
-        thumbnail: {
-            alt: "branding",
-            src: "app/assets/images/xdesign.jpg"
-        },
-        images: [
-            {
-                alt: "branding",
-                src: "app/assets/images/xdesign.jpg"
-            },
-            {
-                alt: "branding",
-                src: "app/assets/images/xdesign.jpg"
-            },
-            {
-                alt: "branding",
-                src: "app/assets/images/xdesign.jpg"
-            }
-        ]
-    },
-    {
-        name: "Web",
-        tagline: "Web apps & web infrastructures",
-        description: "Web apps & web infrastructures",
-        services: ["Web apps", "Websites", "Entreprise software", "Rest APIs"],
-        thumbnail: {
-            alt: "Web",
-            src: "app/assets/images/xdesign.jpg"
-        },
-        images: [
-            {
-                alt: "branding",
-                src: "app/assets/images/xdesign.jpg"
-            },
-            {
-                alt: "branding",
-                src: "app/assets/images/xdesign.jpg"
-            },
-            {
-                alt: "branding",
-                src: "app/assets/images/xdesign.jpg"
-            }
-        ]
-    },
-    {
-        name: "UI & UX",
-        tagline: "UI & UX Design",
-        description: "UI & UX Design",
-        services: ["UI", "UX"],
-        thumbnail: {
-            alt: "UI & UX",
-            src: "app/assets/images/xdesign.jpg"
-        },
-        images: [
-            {
-                alt: "branding",
-                src: "app/assets/images/xdesign.jpg"
-            },
-            {
-                alt: "branding",
-                src: "app/assets/images/xdesign.jpg"
-            },
-            {
-                alt: "branding",
-                src: "app/assets/images/xdesign.jpg"
-            }
-        ]
-    }
+	{
+		name: "Branding",
+		tagline: "visual Identity and branding",
+		description: "visual Identity and branding",
+		services: ["visual Identity", "Print", "branding"],
+		thumbnail: {
+			alt: "branding",
+			src: "app/assets/images/xdesign.jpg"
+		},
+		images: [
+			{
+				alt: "branding",
+				src: "app/assets/images/xdesign.jpg"
+			},
+			{
+				alt: "branding",
+				src: "app/assets/images/xdesign.jpg"
+			},
+			{
+				alt: "branding",
+				src: "app/assets/images/xdesign.jpg"
+			}
+		]
+	},
+	{
+		name: "Web",
+		tagline: "Web apps & web infrastructures",
+		description: "Web apps & web infrastructures",
+		services: ["Web apps", "Websites", "Entreprise software", "Rest APIs"],
+		thumbnail: {
+			alt: "Web",
+			src: "app/assets/images/xdesign.jpg"
+		},
+		images: [
+			{
+				alt: "branding",
+				src: "app/assets/images/xdesign.jpg"
+			},
+			{
+				alt: "branding",
+				src: "app/assets/images/xdesign.jpg"
+			},
+			{
+				alt: "branding",
+				src: "app/assets/images/xdesign.jpg"
+			}
+		]
+	},
+	{
+		name: "UI & UX",
+		tagline: "UI & UX Design",
+		description: "UI & UX Design",
+		services: ["UI", "UX"],
+		thumbnail: {
+			alt: "UI & UX",
+			src: "app/assets/images/xdesign.jpg"
+		},
+		images: [
+			{
+				alt: "branding",
+				src: "app/assets/images/xdesign.jpg"
+			},
+			{
+				alt: "branding",
+				src: "app/assets/images/xdesign.jpg"
+			},
+			{
+				alt: "branding",
+				src: "app/assets/images/xdesign.jpg"
+			}
+		]
+	}
 ];

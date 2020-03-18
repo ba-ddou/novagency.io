634a1e3af79828bcc2c4679fe0bd4f699774c1e2
ba-ddou <ba-ddou@users.noreply.github.com>
2020-03-18 21:57:06 +0100

    nova production firebase config | commented

--- src/app/config/config.js
+++ src/app/config/config.js
@@ -9,4 +9,14 @@ export default {
 		appId: "1:116004573342:web:b2b71e627806f9db1deb8f",
 		measurementId: "G-H68WL5FB8Z"
 	}
+	// firebaseConfig = {
+	// 	apiKey: "AIzaSyB8BJzbnbtz1yYhmzKhXyV68e76S5_Y20I",
+	// 	authDomain: "nova-prod-00.firebaseapp.com",
+	// 	databaseURL: "https://nova-prod-00.firebaseio.com",
+	// 	projectId: "nova-prod-00",
+	// 	storageBucket: "nova-prod-00.appspot.com",
+	// 	messagingSenderId: "97878900285",
+	// 	appId: "1:97878900285:web:653ef09c25e76636e97454",
+	// 	measurementId: "G-6M8G4F9VQR"
+	// }
 };

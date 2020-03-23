90877ebf4fcfa0c0d57253d9105f0407767467c8
ba-ddou <ba-ddou@users.noreply.github.com>
2020-03-23 23:26:39 +0100

    project thumbnail lazy loading init

--- src/app/screens/projects/components/ProjectSlide/index.js
+++ src/app/screens/projects/components/ProjectSlide/index.js
@@ -10,6 +10,7 @@ import "./styles.sass";
 import { Link } from "react-router-dom";
 import { toJS } from "mobx";
 import thumb from "app/assets/images/Essential Stationery.png";
+import { LazyImage } from "react-lazy-images";
 
 // {JSON.stringify(toJS(data))}
 
@@ -18,7 +19,18 @@ const ProjectSlide = ({ data, language }) => {
 		<Link to={`/projects/${data.name}`}>
 			<div key={data.name} className="projectSlide">
 				<div className="projectSlide-thumbnail">
-					<img src={data.thumbnail.src} />
+					<LazyImage
+						src={data.thumbnail.src}
+						alt="projectThumbnail"
+						placeholder={({ imageProps, ref }) => (
+							<img
+								ref={ref}
+								src="https://firebasestorage.googleapis.com/v0/b/nova-dev-00.appspot.com/o/Essential%20Stationery%20small.png?alt=media&token=2e4c471a-82e1-4a09-a364-9e4958e66c98"
+								alt={imageProps.alt}
+							/>
+						)}
+						actual={({ imageProps }) => <img {...imageProps} />}
+					/>
 				</div>
 				<div className="projectSlide-info">
 					<span className="projectSlide-info-name">{data.name}</span>
